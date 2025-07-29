e62edbd760950bbaac056298e206068ec470d365
doohaaa <engk5423@naver.com>
2025-07-29 14:56:52 +0900

    deploy.yml

--- src/main/java/com/example/cicdtest/TestController.java
+++ src/main/java/com/example/cicdtest/TestController.java
@@ -1,4 +1,12 @@
 package com.example.cicdtest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+    @GetMapping("/hi")
+    public String test(){
+        return "짠 ! 내가 만든 서버";
+    }
 }
